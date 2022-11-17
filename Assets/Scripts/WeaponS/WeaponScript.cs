2e941bdbc91bab882973d93e34ceb17ace05e5cf
Koppa Dániel <100847378+KoDan76@users.noreply.github.com>
2022-11-17 17:45:18 +0100

    Bugfixek, zene, reload, menü

---
+++ Assets/Scripts/WeaponS/WeaponScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+
+    public float Offset;//{ get; set; }
+
+    public GameObject Bullet; //{ get; set; }
+    public Transform BarrelPos; //{ get; set; }
+
+    private float timeBtwShots;
+    private float reloadCD;
+    private int magazineCurr;
+
+    public int startMag;
+    public float startTimeBtwShots;
+    public float startReloadTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        magazineCurr = startMag;
+        reloadCD = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + Offset);
+        
+
+        if (timeBtwShots <= 0 && reloadCD <= 0)
+        {
+            
+            if (Input.GetMouseButton(0))
+            {
+                if(magazineCurr <= 0)
+                {
+                    reloadCD = startReloadTime;
+                    magazineCurr = startMag;
+                }
+                Instantiate(Bullet, BarrelPos.position, transform.rotation);
+                magazineCurr -= 1;
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+            reloadCD -= Time.deltaTime;
+        }
+        
+    }
+}
