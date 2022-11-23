9ffd616bb5507b93dbd16dc981848c3c388b4ad0
Rene0514 <rene20000514@gmail.com>
2022-11-23 13:01:28 +0100

    Goal Animation added

--- Assets/Scripts/GoalHandler.cs
+++ Assets/Scripts/GoalHandler.cs
@@ -5,7 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class GoalHandler : MonoBehaviour
 {
-
+    [SerializeField] private float ShakeTime;
+    [SerializeField] private float RightTime;
+    [SerializeField] private float RightSpeed;
+    [SerializeField] private float ShakeDuration;
+    [SerializeField] private float ShakeAmount;
+    private bool LoadNext = false;
+    private bool Shaking = false;
+    private bool GoingRight = false;
     private int nextScene;
     // Start is called before the first frame update
     void Start()
@@ -20,10 +27,56 @@ public class GoalHandler : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        if (LoadNext)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+        if (Shaking)
+        {
+            StartCoroutine(Shake());
+        }
+        if (GoingRight)
+        {
+            transform.position = new Vector3(transform.position.x + (RightSpeed / 100.0f), transform.position.y, transform.position.z);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag != "Player") return;
-        SceneManager.LoadScene(nextScene);
+        collision.gameObject.SetActive(false);
+        StartCoroutine(GoalAnimation());
+        
     }
+    private IEnumerator GoalAnimation()
+    {
+        Shaking = true;
+        yield return new WaitForSeconds(ShakeTime / 100.0f);
+        Shaking = false;
+        GoingRight = true;
+        yield return new WaitForSeconds(RightTime / 100.0f);
+        LoadNext = true;
+    }
+    private IEnumerator Shake()
+    {
+        Vector3 originalPosition = transform.position;
 
+        float elapsed = 0.0f;
+
+        while (elapsed < ShakeDuration)
+        {
+            float y = Random.Range(-1f, 1f) * ShakeAmount;
+
+            transform.position = new Vector3(originalPosition.x, originalPosition.y + y, originalPosition.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.position = originalPosition;
+        
+    }
 }
