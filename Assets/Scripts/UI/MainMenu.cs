a672c7fd63ae30517415b6c6efee83cae5282bad
eckrichard01 <eckrichard01@gmail.com>
2022-12-14 15:40:57 +0100

    map select

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,10 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private int sceneNumber;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if(sceneNumber != 0)
+            SceneManager.LoadScene(sceneNumber);
+        else
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void SaveMap(int map)
+    {
+        sceneNumber = SceneManager.GetActiveScene().buildIndex + map;
+    }
+
     public void Exit()
     {
         Application.Quit();
